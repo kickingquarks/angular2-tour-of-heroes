3b60a0f83b6ac78bc5198c55bb7e144a8bb12839
kickingquarks <kickingquarks@gmail.com>
2016-09-06 18:41:49 -0500

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My Fiftieth Angular 2 App</h1>;<center><h1>I wish I had some ale for my tale</h1></center>;<P align="right"><img src="images/Patriot.jpg"></P>'
+            
+})
+export class AppComponent { }
+
+
