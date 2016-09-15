30d2f02cb3da6f67188f62e2f739801b7425a98a
kickingquarks <kickingquarks@gmail.com>
2016-09-15 15:46:45 -0500

    Step 7 Completed
    Completed Step 7, sweet app

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -28,4 +28,8 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     window.history.back();
   }
+  save(): void {
+  this.heroService.update(this.hero)
+    .then(this.goBack);
+}
 }
