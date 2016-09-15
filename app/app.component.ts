4d9ec7122ee7be4980c7b287226fc707959a0e09
kickingquarks <kickingquarks@gmail.com>
2016-09-15 13:37:06 -0500

    Add files via upload

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My Fiftieth Angular 2 App</h1>;<center><h1>I wish I had some ale for my tale</h1></center>;<P align="right"><img src="images/Patriot.jpg"></P>'
-            
+  template: `
+    <h1>{{title}}</h1>
+    <a routerLink="/heroes">Heroes</a>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent { }
-
-
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
